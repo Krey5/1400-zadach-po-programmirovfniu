7483abd6fc4015eb99aa1e1f8356bc3c86baa55c
Krey5 <110614739+Krey5@users.noreply.github.com>
2025-10-29 18:08:48 +0300

    Delete 2.3.py

--- 2.3.py
+++
@@ -1,9 +0,0 @@
-# а)
-a = float(input("Введите значение a: "))
-result_a = (2*a + (5*a)) / 3.56
-print(result_a)
-
-# б)
-x = float(input("Введите значение x: "))
-result_b = ((5.2 + (1 + x)**0.5) / (15 * x))
-print(result_b)
