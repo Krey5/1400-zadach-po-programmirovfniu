91a46a9a638368573f9a9e422c078f86e9876cdf
Krey5 <110614739+Krey5@users.noreply.github.com>
2025-10-29 18:14:14 +0300

    Add files via upload

---
+++ 2.3.py
@@ -0,0 +1,11 @@
+import math
+
+# а)
+a = float(input("Введите значение a: "))
+r_a = (2*a + math.sin(5*a)) / 3.56
+print(r_a)
+
+# б)
+x = float(input("Введите значение x: "))
+r_b = math.sin((5.2 + (1 + x)**0.5) / (15 * x))
+print(r_b)
