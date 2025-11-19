775009cf081029b5f25bc9e93f24e1a23ed7ae08
Krey5 <110614739+Krey5@users.noreply.github.com>
2025-11-19 19:56:31 +0300

    Add files via upload

---
+++ 2.22.py
@@ -0,0 +1,13 @@
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+
+abs_a = abs(a)
+abs_b = abs(b)
+
+arif = (abs_a + abs_b) / 2
+
+geom = (abs_a * abs_b) ** 0.5
+
+
+print(arif)
+print(geom)
