64421026254bc057078367b8d07605a09e240378
Krey5 <110614739+Krey5@users.noreply.github.com>
2025-12-02 21:15:52 +0300

    Add files via upload

---
+++ 2.38.py
@@ -0,0 +1,8 @@
+a = int(input("Первое число: "))
+b = int(input("Второе число: "))
+
+print(a, "+", b, "=", a + b)
+print(a, "-", b, "=", a - b)
+print(a, "*", b, "=", a * b)
+print(a, "/", b, "=", a / b)
+print("(", a, "+", b, ")/2 =", (a + b) / 2)
