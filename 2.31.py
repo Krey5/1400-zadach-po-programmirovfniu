d6a6b1d8a40bbc867af1a397010a0bb60faeb03f
Krey5 <110614739+Krey5@users.noreply.github.com>
2025-12-02 20:18:11 +0300

    Add files via upload

---
+++ 2.31.py
@@ -0,0 +1,11 @@
+candy = float(input("Стоимость 1 кг конфет: "))
+cookie = float(input("Стоимость 1 кг печенья: "))
+apple = float(input("Стоимость 1 кг яблок: "))
+
+x = float(input("Купили конфет (кг): "))
+y = float(input("Купили печенья (кг): "))
+z = float(input("Купили яблок (кг): "))
+
+summ = candy * x + cookie * y + apple * z
+
+print(summ)
